1c97dd449213a1c3c53e699da9699decf8ea7903
Jack <jacksandeman@hotmail.co.uk>
2021-12-14 15:59:18 +0000

    Made it so webpack hot reloads and hosts files

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   entry: ['./src/js/index.js', './src/scss/index.scss'],
   output: {
     filename: './js/main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './public/dist'),
+    publicPath: '/public/',
   },
   module: {
     rules: [
@@ -28,5 +29,17 @@ module.exports = {
         ],
       },
     ],
+  },
+  devServer: {
+    static: { directory: path.join(__dirname, 'public'), watch: true },
+    client: {
+      logging: 'none',
+      overlay: { errors: true, warnings: false },
+    },
+    compress: true,
+    port: 3000,
+    devMiddleware: {
+      writeToDisk: true,
+    },
   },
 };
